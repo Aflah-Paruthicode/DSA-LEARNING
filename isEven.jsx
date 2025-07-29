e841c155abe8eb1a31cf0446a3287cf268308b4f
Aflah Paruthicode <aflutech2@gmail.com>
2025-07-29 22:13:28 +0530

    isEvenOrOdd

---
+++ isEven.jsx
@@ -0,0 +1,11 @@
+function checkEvenOrOdd (number) {
+    if(number % 2 == 0) {
+        console.log(number+' Is a even number');
+    } else {
+        console.log(number+' Is a odd number');
+    };
+};
+
+checkEvenOrOdd(2);
+checkEvenOrOdd(5);
+checkEvenOrOdd(9);
