bd8baf18294e47e5b8efb1917c18a1fc4154c2ac
Aflah Paruthicode <aflutech2@gmail.com>
2025-08-05 18:11:17 +0530

    loop inside loop2

--- warmUp/loop.js
+++ warmUp/loop.js
@@ -1,6 +1,17 @@
+
+// loop 1
 for( let i = 0 ; i < 5 ; i ++ ) {
     for( j = 0 ; j < 5 ; j ++) {
         console.log('I : '+i+" J : "+j);
     }
 }
+
+
+// loop 2
+for( let i = 0 ; i < 3 ; i ++ ) {
+    for( j = 0 ; j < i ; j ++) {
+        console.log('I : '+i+" J : "+j);
+    }
+}
+
 
