34ce309a702d6a970c7848e292fa50962196cbe8
Aflah Paruthicode <aflutech2@gmail.com>
2025-08-06 12:19:16 +0530

    Inverted Right Triangle Star  Pattern

---
+++ warmUp/patternInvertedRightTriaStar.js
@@ -0,0 +1,13 @@
+for(let i = 0;i<5;i++) {
+    let row = '';
+    for(let j = 0; j<5-i; j++) {
+        row += ` ★`;
+    }
+    console.log(row);
+}
+
+//  ★ ★ ★ ★ ★
+//  ★ ★ ★ ★
+//  ★ ★ ★
+//  ★ ★
+//  ★
