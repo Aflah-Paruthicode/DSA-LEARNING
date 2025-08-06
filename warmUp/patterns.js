2635ddfb8d235bd0f311726fee5d4e57a3897552
Aflah Paruthicode <aflutech2@gmail.com>
2025-08-06 10:48:59 +0530

    square pattern

---
+++ warmUp/patterns.js
@@ -0,0 +1,15 @@
+let num = 5
+
+for( let i = 0 ; i < num ; i ++ ) {
+    let row = ''
+    for( j = 0 ; j < num ; j ++ ) {
+        row+= ' ★' ;
+        }
+        console.log(row);
+}
+
+//  ★ ★ ★ ★ ★
+//  ★ ★ ★ ★ ★
+//  ★ ★ ★ ★ ★
+//  ★ ★ ★ ★ ★
+//  ★ ★ ★ ★ ★
