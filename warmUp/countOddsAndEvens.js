95aceed56f6e7d062ffebd3e9297c7f75f6583a9
Aflah Paruthicode <aflutech2@gmail.com>
2025-08-08 23:14:09 +0530

    Count Odds And Evens

---
+++ warmUp/countOddsAndEvens.js
@@ -0,0 +1,15 @@
+function CountOddsAndEvens (num) {
+    let odds = 0;
+    let evens = 0;
+    
+    while(num > 0) {
+        let rem = num%10;
+        rem%2 == 0 ? evens++ : odds++;
+        num = Math.floor(num/10);
+    }
+    return [odds,evens]
+};
+
+let number = 24561;
+
+console.log(CountOddsAndEvens(number));
