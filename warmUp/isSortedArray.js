ce8f57bbdd5f63eda944fbd7e19e57eb764357f3
Aflah Paruthicode <aflutech2@gmail.com>
2025-08-12 23:30:06 +0530

    Is Sorted Array Renewed

--- warmUp/isSortedArray.js
+++ warmUp/isSortedArray.js
@@ -1,10 +1,18 @@
-function isSortedArray (arr) {
-    for(let i = 1;i<arr.length;i++) {
-        if(arr[i-1] > arr[i]) {
-            return false;
+function isSortedArr (arr) {
+    if(arr.length < 2) return 'Need minimum two elements to work this algorthim';
+    let isInc = arr[0] < arr[1] ? true : false;
+    let seq = isInc ? arr[1]-arr[0] : arr[0]-arr[1];
+    let count = 0;
+    
+    for(let i = 0;i<arr.length;i++) {
+        if(arr[i+1] == arr[i]+seq || arr[i+1] == arr[i]-seq) {
+            count++;
         }
     }
-    return true
+    return count == arr.length-1 ? true : false;
 }
 
-console.log(isSortedArray([1,22,3,34]))
+console.log(isSortedArr([5,4,3,2,1]))
+console.log(isSortedArr([1,2,3,4,5]))
+console.log(isSortedArr([1,3,5,7]))
+console.log(isSortedArr([]))
